1731df3657c09d5ffba2338f1d98cbe9493c26cd
fachryaa <fajri.fr85@gmail.com>
2023-01-16 17:14:03 +0700

    Add Feature: Detail Post

--- controllers/posts.js
+++ controllers/posts.js
@@ -17,6 +17,18 @@ export const getPosts = async (req,res) => {
   }
 }
 
+export const getPostById = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const post = await PostMessage.findById(id)
+
+    res.status(200).json(post)
+  } catch (error) {
+    res.status(404).json({ message: "Post don't exist" })
+  }
+}
+
 // QUERY -> /posts?page=1 -> page = 1
 // PARAMS -> /posts/:id -> /posts/123 -> id = 123
 
--- routes/posts.js
+++ routes/posts.js
@@ -1,12 +1,14 @@
 import express from 'express';
 
-import { getPostsBySearch, getPosts, createPost, updatePost, deletePost, likePost } from '../controllers/posts.js';
+import { getPostsBySearch, getPosts, getPostById, createPost, updatePost, deletePost, likePost } from '../controllers/posts.js';
 
 import auth from '../middleware/auth.js';
 const router = express.Router();
 
 router.get('/search', getPostsBySearch);
 router.get('/', getPosts);
+router.get('/:id', getPostById)
+
 router.post('/', auth, createPost);
 router.patch('/:id', auth, updatePost);
 router.delete('/:id', auth, deletePost)
