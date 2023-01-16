db00bd3410ae20a22d3db850f3d22b0c636ec052
fachryaa <fajri.fr85@gmail.com>
2023-01-16 23:34:10 +0700

    test deploy

--- controllers/posts.js
+++ controllers/posts.js
@@ -91,7 +91,6 @@ export const deletePost = async (req, res) => {
 
 export const likePost = async (req, res) => {
   const { id } = req.params;
-  // console.log(req)
 
   if (!req.userId) return res.json({ message: 'Unauthenticated' });
 
